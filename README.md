24da39543c5a0acbf7ad775148d1016ee5277816
Tabitha <tabithahsia@gmail.com>
2018-02-20 12:23:41 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Lob Coding Challenge
+
+Build a command line program to send a letter to your legislator with Lob.
+
+## Example
+![Screenshot Example of Input](https://ibb.co/kNXRBm)
+![Screenshot Example of Output](https://ibb.co/hgAUWm)
+
+## Getting Started
+
+### Requirements
+- Node 9.2.0
+
+### Installing System Dependencies
+Please visit https://nodejs.org/en/ to download the latest version of Node.
+
+### Installing Project Dependencies
+From within the root directory:
+
+```sh
+
+npm install
+
+```
+### To Run the Program
+
+`node index.js`
+
+## Built With
+- [Node](https://nodejs.org/en/)
+- [Lob](https://lob.com/)
+- [Axios](https://github.com/axios/axios)
+- [Inquirer](https://github.com/SBoudrias/Inquirer.js/)
---
+++ index.js
@@ -0,0 +1,120 @@
+'use strict';
+
+//API KEYS
+const keys = require('./keys.js');
+
+//COMMAND LINE INTERFACE
+var inquirer = require('inquirer');
+
+//LOB
+var Lob = require('lob')(keys.LOB_API_KEY);
+
+var axios = require('axios');
+var fs = require('fs');
+var file = fs.readFileSync(__dirname + '/letter.html').toString();
+
+var myInfo, myAddress, legislatorInfo;
+
+console.log('Write a letter to your legislator!');
+
+var questions = [
+  {
+    type: 'input',
+    name: 'fullName',
+    message: 'What is your full name?',
+  },
+  {
+    type: 'input',
+    name: 'addressLine1',
+    message: 'What is the first line of your address?'
+  },
+  {
+    type: 'input',
+    name: 'addressLine2',
+    message: 'What is the second line of your address? (or press Enter to skip)',
+  },
+  {
+    type: 'input',
+    name: 'city',
+    message: 'What is your city?'
+  },
+  {
+    type: 'input',
+    name: 'state',
+    message: 'What is your state?'
+  },
+  {
+    type: 'input',
+    name: 'zipcode',
+    message: 'What is your zipcode?',
+    validate: function(value) {
+      var pass = value.match(
+        /^\d{5}$|^\d{5}-\d{4}$/
+      );
+      if(pass) {
+        return true;
+      }
+      return 'Please enter a valid zipcode';
+    }
+  },
+  {
+    type: 'input',
+    name: 'messageToSend',
+    message: 'What would you like to say?'
+  }
+];
+
+inquirer.prompt(questions).then(answers => {
+  myInfo = answers;
+  myAddress = answers.addressLine1 + ' ' + answers.addressLine2 + ', ' + answers.city + ', ' + answers.state + ' ' + answers.zipcode
+})
+.then(() => {
+  axios.get('https://www.googleapis.com/civicinfo/v2/representatives?key=' + keys.GOOGLE_API_KEY + '&address=' + myAddress)
+   .then(function(response){
+     legislatorInfo = response.data.officials[2];
+   })
+   .catch(error => {
+    // console.log('There is an error: ', error);
+    console.log('The address you entered is not valid');
+   })
+   .then(() => {
+     Lob.addresses.create({
+       name: legislatorInfo.name,
+       address_line1: legislatorInfo.address[0].line1,
+       address_city: legislatorInfo.address[0].city,
+       address_state: legislatorInfo.address[0].state,
+       address_zip: legislatorInfo.address[0].zip,
+       address_country: 'US'
+     })
+     .then(address => {
+       // console.log('here is the address object', address);
+       return Lob.letters.create({
+         description: 'Letter to legislator',
+         to: address.id,
+         from: {
+           name: myInfo.fullName,
+           address_line1: myInfo.addressLine1,
+           address_line2: myInfo.addressLine2,
+           address_city: myInfo.city,
+           address_state: myInfo.state,
+           address_zip: myInfo.zipcode,
+           address_country: 'US',
+         },
+         file: file,
+         merge_variables: {
+           legislatorName: legislatorInfo.name,
+           myName: myInfo.fullName,
+           message: myInfo.messageToSend,
+           date: address.date_created.slice(0, 10)
+         },
+         color: false
+       });
+     })
+     .then(letter => {
+       console.log('here is the URL to your letter:', letter.url);
+     })
+     .catch(err => {
+       console.log(err);
+     });
+   });
+})
